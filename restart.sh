f8904e1d5e7e3e5e232b40a8b8ced386eb32bfc4
Patrick Riegler <patrick@patricks-macbook-pro.home>
2018-06-21 13:57:27 +0200

    extract test.sh script from restart.sh

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 
 host=http://localhost:8081
-# host=http://ecs-first-run-alb-1659585136.eu-central-1.elb.amazonaws.com
+# host=http://ecs-first-run-alb-1772213034.eu-central-1.elb.amazonaws.com
 
 echo
 echo "restarting the service"
@@ -10,35 +10,10 @@ echo
 echo "test if service was started"
 bash -c 'i=0; while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' '${host}'/vin/check/WAUZZZF50JN016611)" != "200" && $((i++)) -lt 10 ]]; do echo $(date); sleep 1; done'
 
-echo
-echo "run health check:"
-url="${host}/healthcheck"
-echo "testing (debug): $i --- ($url)"
-curl $url
 
-echo
-echo "run test cases:"
-url="${host}/vin/check/WAUZZZF50JN016611?debug=true&test=true"
-echo "testing (debug): $i --- ($url)"
-curl $url
-echo
-testcases="$(cat api/positivetestcases.test)"
-for i in $testcases
-do
-  echo "testing: $i"
-  url="${host}/vin/check/$i?test=true"
-  echo "testing: $i - check --- ($url)"
-  curl $url
-  echo
-  url="${host}/vin/decode/$i?test=true"
-  echo "testing: $i - decode --- ($url)"
-  curl $url
-  echo
-done
-
-# echo "testing (vin/findInImage): $i"
-# curl "${host}/vin/findInImage"
-# echo
+# run the test cases
+#./test.sh full
+./test.sh 
 
 
 echo
---
+++ test.sh
@@ -0,0 +1,40 @@
+
+# host=http://localhost:8081
+host="ripro-svis-lb-96fd9452b00ef973.elb.eu-central-1.amazonaws.com"
+
+echo
+echo "run health check:"
+url="${host}/healthcheck"
+echo "testing (debug): $i --- ($url)"
+curl $url
+
+echo
+echo "run test cases:"
+url="${host}/vin/check/WAUZZZF50JN016611?debug=true&test=true"
+echo "testing (debug): $i --- ($url)"
+curl $url
+echo
+
+if [ "$1" = "full" ]; then
+  testcases="$(cat api/positivetestcases.test.full)"
+else
+  testcases="$(cat api/positivetestcases.test)"
+fi
+
+for i in $testcases
+do
+  echo "testing: $i"
+  url="${host}/vin/check/$i?test=true"
+  echo "testing: $i - check --- ($url)"
+  curl $url
+  echo
+  url="${host}/vin/decode/$i?test=true"
+  echo "testing: $i - decode --- ($url)"
+  curl $url
+  echo
+done
+
+# echo "testing (vin/findInImage): $i"
+# curl "${host}/vin/findInImage"
+# echo
+
