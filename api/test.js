d794777e1b8e6e7509b15d5a4de79b2ec29a620a
Patrick Riegler <patrick.riegler@intdev.ch>
2018-07-09 08:43:13 +0000

    extend test cases with real mocha REST API test case

--- api/test.js
+++ api/test.js
@@ -1,11 +1,31 @@
+let mongoose = require("mongoose");
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+//let server = require('server');
+let should = chai.should();
+
 var assert = require('assert');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
+var SERVERN = (process.env.SERVERN!=undefined)? process.env.SERVERN : "localhost";
+var PORT = (process.env.PORT>0)? process.env.PORT : 80;
+
+console.log
+
+chai.use(chaiHttp);
+
+  describe('/vin/check verify vin number WAUZZZF50JN016611', () => {
+      it('verify if VIN WAUZZZF50JN016611 is valid', (done) => {
+        console.log("http://"+SERVERN+":"+PORT)
+        chai.request("http://"+SERVERN+":"+PORT)
+            .get('/vin/check/WAUZZZF50JN016611')
+            .end((err, res) => {
+console.log("res: "+res)
+                res.should.have.status(200);
+              done();
+            });
+      });
   });
-});
+
+
 
 // include further tests here
