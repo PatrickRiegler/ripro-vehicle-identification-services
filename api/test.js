3823a70dfcb4e9d7ac929295515f84d2203e508a
tiborhajdu <43000378+tiborhajdu@users.noreply.github.com>
2018-09-17 17:09:33 +0300

    Add files via upload

--- api/test.js
+++ api/test.js
@@ -1,22 +1,36 @@
-var request = require('request');
-request('http://www.google.com', function(err, res) {
-    console.log('err:', err);
-    console.log('statusCode:', res &&res.statusCode);
-   });
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {   
-    it('should return -1 when http://www.google.com not present', function () {
-        assert([200,404].indexOf(3), -1);
-    });
-  });
-});
-      
+var chai = require('chai')
+  , chaiHttp = require('chai-http');
 
-  describe('prove, that htttp response code is 200', function() {
-    it('this test proves that http response code is 200', function() {
-      assert(200 == 200);
-    });
+var assert = require('chai').assert
+var expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Array', function() {
+    describe('indexOf', function() {
+        it('should return -1 when the value not present', function() {
+            chai.request('http://www.google.com')
+               .get('/')
+               .end(function(error,response) {
+                 expect(error).to.be.null;
+                 expect(response).to.have.status(200);
+                 assert.equal([200,response.status].indexOf(3), -1);
+               });
+        });
+     });
+});
+
+   describe('prove that, the response code is 200', function() {
+      it('this test proves, that the response code is 200', function(done) {
+         chai.request('http://www.google.com')
+              .get('/')
+              .end(function(error, response) {
+                 expect(error).to.be.null;
+                 expect(response).to.have.status(200);
+                 assert.equal(200,response.status);
+                 done();
+              });
+      });
   });
 
     // test branch restriction
