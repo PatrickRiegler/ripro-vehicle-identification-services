028d6d6fc22945356d8e8a8064c798e969e344a6
Patrick Riegler <patrick@patricks-macbook-pro.home>
2018-09-18 09:57:08 +0200

    make test cases in a loop to run multiple test cases

--- api/test.js
+++ api/test.js
@@ -4,37 +4,26 @@ var chai = require('chai')
 var assert = require('chai').assert
 var expect = chai.expect;
 
+var serverPort = (process.env.PORT>0)? process.env.PORT : 8081;
+// console.log(serverPort)
 chai.use(chaiHttp);
+url="http://localhost:"+serverPort
+chairequest = chai.request(url)
 
-describe('Array', function() {
-    describe('indexOf', function() {
-        it('should return -1 when the value not present', function() {
-            chai.request('http://www.google.com')
-               .get('/')
-               .end(function(error,response) {
-                 expect(error).to.be.null;
-                 expect(response).to.have.status(200);
-                 assert.equal([200,response.status].indexOf(3), -1);
-               });
+testCases=["/healthcheck","/vin/check/WAUZZZF50JN016611"]
+
+describe('prove that, the response code is 200', function() {
+  testCases.forEach(function(tc) {
+    it('this test proves, that the response code for '+url+tc+' is 200', function(done) {
+      // console.log(url+tc)
+      chairequest.get(tc)
+        .end(function(error, response) {
+          expect(error).to.be.null;
+          expect(response).to.have.status(200);
+          assert.equal(200,response.status);
+          done();
         });
-     });
+    });
+  })
 });
-   describe('prove that, the response code is 200', function() {
-      it('this test proves, that the response code is 200', function(done) {
-         chai.request('http://www.google.com')
-              .get('/')
-              .end(function(error, response) {
-                 expect(error).to.be.null;
-                 expect(response).to.have.status(200);
-                 assert.equal(200,response.status);
-                 done();
-              });
-      });
-  });
 
-    // test branch restriction
- 
-    // tibors new code
-  describe('Tibors Test Cases', function() {
-    console.log("Tibors log entry");
-  });
